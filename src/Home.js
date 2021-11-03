92b3317f459e2941a0487511036bf39abc35a1e6
Nihal padwal <nihalpadwal@gmail.com>
2021-11-03 12:11:07 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -14,54 +14,47 @@ function Home() {
 
         <div className="home__row">
           <Product
-            id="12321341"
-            title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
-            price={11.96}
-            rating={5}
-            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
-          />
-          <Product
-            id="49538094"
-            title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
-            price={239.0}
-            rating={4}
-            image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
-          />
+              title="The lean stratup"
+              image="https://m.media-amazon.com/images/I/51WIKlio9qL._SY346_.jpg"
+              price={2000}
+              rating={5}
+            />
+            <Product
+              title="AORUS Gigabyte Nvidia GeForce RTX 3070 AORUS Master 8GB GDDR6 Graphics Card (GV-N3070AORUS M-8GD)"
+              image="https://m.media-amazon.com/images/I/71If+Mn0ByL._SL1500_.jpg"
+              price={80000}
+              rating={5}
+            />
+            <Product
+              title="AMD 5000 Series Ryzen 7 5800X Desktop Processor 8 cores 16 Threads 36 MB Cache 3.8 GHz Upto 4.7 GHz AM4 Socket 500 Series Chipset (100-100000063WOF)"
+              image="https://m.media-amazon.com/images/I/61DYLoyNRWL._SL1384_.jpg"
+              price={37280.0}
+              rating={5}
+              />
         </div>
 
         <div className="home__row">
           <Product
-            id="4903850"
-            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
-            price={199.99}
-            rating={3}
-            image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
-          />
-          <Product
-            id="23445930"
-            title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
-            price={98.99}
-            rating={5}
-            image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
-          />
-          <Product
-            id="3254354345"
-            title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
-            price={598.99}
-            rating={4}
-            image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
-          />
+              title="Logitech G512 Mechanical Gaming Keyboard,RGB Lightsync Backlit Keys,GX Brown Tactile Key Switches,Brushed Aluminum Case,Customizable F-Keys,USB Pass Through - Black"
+              image="https://m.media-amazon.com/images/I/514VCacbvLL._SL1024_.jpg"
+              price={5000}
+              rating={5}
+            />
+            <Product
+              title="Logitech G502 Hero High Performance Wired Gaming Mouse, Hero 25K Sensor, 25,000 DPI, RGB, Adjustable Weights, 11 Programmable Buttons, On-Board Memory, PC/Mac - Black"
+              image="https://m.media-amazon.com/images/I/61mpMH5TzkL._SL1500_.jpg"
+              price={4380.0}
+              rating={5}
+            />
         </div>
-
-        <div className="home__row">
-          <Product
-            id="90829332"
-            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
-            price={1094.98}
-            rating={4}
-            image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
-          />
-        </div>
+        <div className="home_row">
+            <Product
+              title="Samsung LC49HG90DMUXEN 48.9-inch Ultra Wide Curved Monitor (Black)"
+              image="https://m.media-amazon.com/images/I/71MlcO29QOL._SL1500_.jpg"
+              price={158900.0}
+              rating={5}
+            />
+          </div>
       </div>
     </div>
   );
